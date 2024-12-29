13499e6bc1172819137b73bf776ddb2cc9d1e7ae
Abderrahmane Mihoubi <mihoubiabderrahmane09@gmail.com>
2024-12-29 20:48:17 +0100

    Completing Chapter 4th Scanning

--- vic/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ vic/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -3,6 +3,7 @@ package com.craftinginterpreters.lox;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,20 +11,24 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
             System.exit(64);
         } else if (args.length == 1) {
-            //
+            runFile(args[0]);
         } else {
-            //
+            runPrompt();
         }
     }
 
     public static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
-        //run method
+        run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(64);
     }
 
     public static void runPrompt() throws IOException {
@@ -34,12 +39,26 @@ public class Lox {
             System.out.println("> ");
             String line = reader.readLine();
             if (line == null) break;
-            //run method
+            run(line);
+            hadError = false;
         }
     }
 
     public static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner();
+        com.craftinginterpreters.lox.Scanner scanner = new com.craftinginterpreters.lox.Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
